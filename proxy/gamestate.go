3ac702b2a552220cd522ba385b97e6ba74336116
chew01 <chewjh19@gmail.com>
2022-07-01 15:47:50 +0800

    Tweaking ping

--- proxy/gamestate.go
+++ proxy/gamestate.go
@@ -4,15 +4,13 @@ type GameState struct {
 	Player    *PlayerData
 	Character *CharacterData
 	Activity  *ActivityData
-	Hook      chan *GameState
+	PingFn    func(state *GameState)
 }
 
-func (gs *GameState) SetHook(hook chan *GameState) {
-	gs.Hook = hook
+func (gs *GameState) SetFn(fn func(state *GameState)) {
+	gs.PingFn = fn
 }
 
 func (gs *GameState) Ping() {
-	if gs.Hook != nil {
-		gs.Hook <- gs
-	}
+	gs.PingFn(gs)
 }
